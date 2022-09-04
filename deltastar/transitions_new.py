3684d88934a7f3e08e636b154f730877aecc9b82
Eric <eric.sclafani321@gmail.com>
2022-09-04 15:44:48 -0400

    rule tweaks

--- deltastar/transitions_new.py
+++ deltastar/transitions_new.py
@@ -8,28 +8,36 @@ class Rule:
     insym:str
     outsym:str
     context:list
-    ctype:str
-    k:int
+    ctype:str # context type
+    mtype:str # map type
+    mindex:int # map index
+    
+    def __repr__(self):
+        return f"{self.insym} -> {self.outsym} / {' '.join(self.context)}"
     
     @property
     def statelabels(self):
-        return subslices(self.context)
+        
+        if self.ctype == "left":
+            context = self.context[:-1]
+            
+        return subslices(context) # exclude the underscore
     
     @classmethod
     def rule(cls, mapping:tuple[str], context):
         
-        insym, outsym = mapping[0], mapping[1]
+        insym, outsym = mapping
         context = context.split()
-        
         index = context.index("_")
-        ctype = "left" if index == len(context)-1 else "right" if index == 0 else "dual"
         
-        k = len(context)
+        ctype = "left" if index == len(context)-1 else "right" if index == 0 else "dual"           
+        mtype = "delete"if not outsym else "insert" if not insym else "rewrite"  
         
-        return cls(insym, outsym, context, ctype, k)
+        return cls(insym, outsym, context, ctype, mtype, index)
     
     
-rule = Rule.rule(("a", "b"), "a c a b _")
+rule = Rule.rule(("b", "d"), "a c a b _")
+print(rule.statelabels)
 
 
 
